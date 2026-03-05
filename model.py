e4bb643d9321da7186bc54b3fe509a040c40147a
Yogesh-0904 <yogeshsakthivel07@gmail.com>
2026-03-05 13:58:52 +0530

    Initial project commit

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-5[5rcimport pandas as pd
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_absolute_error
